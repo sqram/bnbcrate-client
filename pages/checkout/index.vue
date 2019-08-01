ecbc9af299c7ae43f8c46ac43565b9249bb5efa5
= <bruce83@gmail.com>
2019-08-01 18:23:05 -0400

    netlify.toml update

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -479,7 +479,8 @@ export default {
 
   async mounted()
   {    
-    
+    console.log('Stripe....')
+    console.log(process.env.STRIPE_PUBLIC_KEY)
     // Stripe element styling    
     window.stripe = window.Stripe(process.env.STRIPE_PUBLIC_KEY);
     var elements = stripe.elements();    
